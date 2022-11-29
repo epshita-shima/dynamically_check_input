4d1ede363cc614863e8d293d850131a36849b20c
epshita-shima <epshitashima@gmail.com>
2022-11-30 00:14:30 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState([])
+  console.log(count)
+  const addFields = (e) => {
+    e.preventDefault()
+    console.log(e)
+    const number = (e.target.member.value)
+    const array = []
+
+    for (var i = 0; i < number; i++) {
+      array.push([i])
+
+    }
+    console.log(array)
+    setCount(array)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={addFields}>
+
+        <input type="text" id="member" name="member" />
+        <button type='submit'>Fill Details</button>
+        <input type="text" />
+        {
+          count.map(item => {
+            return (
+              <div>
+                <input type="checkbox" id="check" name="check" />
+                <input type="text" id="price" name="price" />
+              </div>
+            )
+          })
+        }
+      </form>
     </div>
   );
 }
