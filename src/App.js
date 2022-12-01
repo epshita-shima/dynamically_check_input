f4d63691bcb05b44b4828d04ea154d80a85a0934
epshita-shima <epshitashima@gmail.com>
2022-12-01 07:37:19 +0600

    show output

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import "./App.css";
 import { useState } from "react";
 import { Formik } from "formik";
@@ -9,24 +8,51 @@ function App() {
   const [totalPrice, setTotalPrice] = useState([])
   const [position, setPosition] = useState([])
   const [singleTotalPrice, setSingleTotalPrice] = useState([])
-  console.log(position);
+  const [isShow, setIsshow] = useState(false)
   const addFields = (e) => {
-    e.preventDefault();
-    console.log(e);
-    const number = e.target.member.value;
+    const { value, checked } = e.target;
+    console.log(value, checked)
 
-    const array = [];
-    for (var i = 0; i < number; i++) {
-      array.push([i]);
+
+    // {
+    //   values.priceData?.map((detail, index) => {
+    //     var element;
+    //     let getTotalPrice = 0;
+
+    //     for (let i = 0; i < values.priceData.length; i++) {
+    //       element = values.priceData[i].price;
+    //       console.log(element);
+    //       if (checked) {
+    //         getTotalPrice += +element;
+    //       } else {
+    //         getTotalPrice = 0;
+    //       }
+    //       console.log("totalPrice=", getTotalPrice);
+    //       setTotalPrice(getTotalPrice)
+    //     }
+    {
+      count.map((item, index) => {
+        var form = document.forms[0];
+        var selectElement = form.querySelector(
+          `input[name="item.${index}.check"]`
+        );
+        var selectMemberElement = form.querySelector(
+          `input[name="member"]`
+        );
+
+        if (checked) {
+          selectElement.checked = true;
+        } else {
+          selectElement.checked = false;
+        }
+      })
     }
-    setCount(array);
-    count.map((item, index) => {
-      var form = document.forms[0];
-      var selectElement = form.querySelector(
-        `input[name="item.${index}.price"]`
-      );
-    });
+    //   });
+    // }
   };
+  const calculate = () => {
+
+  }
   return (
     <div className="w-screen h-screen p-10 bg-gray-50">
       <h2 className="max-w-md mx-auto text-center text-2xl text-red-700 font-bold">
@@ -40,10 +66,27 @@ function App() {
             },
           ],
         }}
-        render={({ values, setFieldValue }) => (
+        onSubmit={(values, actions) => {
+          console.log(values)
+          const number = values.data;
+          console.log(number)
+          const array = [];
+          for (var i = 0; i < number; i++) {
+            array.push([i]);
+          }
+          setCount(array);
+          count.map((item, index) => {
+            var form = document.forms[0];
+            var selectElement = form.querySelector(
+              `input[name="item.${index}.price"]`
+            );
+          });
+        }}
+
+        render={({ values, setFieldValue, handleSubmit }) => (
           <div className="mx-w-md mx-auto mt-10 space-y-5">
             <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-              <form onSubmit={addFields}>
+              <form onSubmit={handleSubmit}>
                 <label htmlFor="" className="text-[18px] font-semibold mr-2">
                   No of textbox
                 </label>
@@ -51,6 +94,10 @@ function App() {
                   type="text"
                   id="member"
                   name="member"
+                  onChange={(e) => {
+                    console.log(e.target.value)
+                    setFieldValue('data', e.target.value)
+                  }}
                   className="input border border-black h-[38px] text-center"
                 />
 
@@ -58,54 +105,40 @@ function App() {
                   type="submit"
                   className="bg-[#86198f] text-white px-3 py-2 rounded shadow ml-4"
                 >
-                  Fill Details
+                  Add textbox
                 </button>
                 <br />
                 <div class="p-4 h-auto flex items-center justify-center space-y-1 bg-white rounded shadow">
                   <input
                     type="checkbox"
                     className="w-[35px] h-[20px] mt-2"
-                    onClick={(e) => {
-                      const { value, checked } = e.target;
-                      {
-                        values.priceData?.map((detail, index) => {
-                          var element;
-                          let getTotalPrice = 0;
+                    onClick={(e,) => {
 
-                          for (let i = 0; i < values.priceData.length; i++) {
-                            element = values.priceData[i].price;
-                            console.log(element);
-                            if (checked) {
-                              getTotalPrice += +element;
-                            } else {
-                              getTotalPrice = 0;
-                            }
-                            console.log("totalPrice=", getTotalPrice);
-                            setTotalPrice(getTotalPrice)
+                      const { value, checked } = e.target
+                      addFields(e)
+                      console.log(values)
+
+                      values.priceData?.map((detail, index) => {
+                        var element;
+                        let getTotalPrice = 0;
+
+                        for (let i = 0; i < values.priceData.length; i++) {
+                          element = values.priceData[i].price;
+                          console.log(element);
+                          if (checked) {
+                            getTotalPrice += +element;
+                          } else {
+                            getTotalPrice = 0;
                           }
-                          {
-                            count.map((item, index) => {
-                              var form = document.forms[0];
-                              var selectElement = form.querySelector(
-                                `input[name="item.${index}.check"]`
-                              );
-                              var selectMemberElement = form.querySelector(
-                                `input[name="member"]`
-                              );
-                              setSelectAll(selectMemberElement.value)
-                              console.log(selectElement)
-                              if (checked) {
-                                selectElement.checked = true;
-                              } else {
-                                selectElement.checked = false;
-                              }
-                            })
-                          }
-                        });
-                      }
+                          console.log("totalPrice=", getTotalPrice);
+                          setTotalPrice(getTotalPrice)
+                          setIsshow(isShow)
+                        }
+                      })
+
                     }}
                   />
-                  <label htmlFor="" className="text-[20px] font-semibold mr-2">
+                  <label label htmlFor="" className="text-[20px] font-semibold mr-2" >
                     All Check
                   </label>
                 </div>
@@ -126,6 +159,7 @@ function App() {
                           else {
                             setPosition([...position, null])
                           }
+
                           var element;
                           var getSingleTotalPrice = 0;
 
@@ -139,7 +173,7 @@ function App() {
                             temp[index] = selectElement.value;
                             setPriceValue(temp);
                             for (let i = 0; i < temp.length; i++) {
-                              var element = temp[i];
+                              element = temp[i];
                               getSingleTotalPrice += + element
                               console.log(getSingleTotalPrice)
                             }
@@ -153,11 +187,12 @@ function App() {
                             temp[index] = 0;
                             setPriceValue(temp);
                             for (let i = 0; i < temp.length; i++) {
-                              var element = temp[i];
+                              element = temp[i];
                               getSingleTotalPrice += + element
                               console.log(getSingleTotalPrice)
                             }
                             setSingleTotalPrice(getSingleTotalPrice)
+                            setIsshow(!isShow)
                           }
                         }}
                         className="w-[35px] h-[20px] mt-1"
@@ -171,22 +206,27 @@ function App() {
                             `priceData.${index}.price`,
                             e.target.value
                           );
+                          console.log(values)
                         }}
                         className="input border border-black h-[38px] text-center mt-1 ml-4"
                       />
+
                     </div>
                   );
                 })}
                 <div>
-                  <p>selectted all {selectAll} item and Total number is {totalPrice}</p>
+                  {/* <p>selectted all {count} item and Total number is {totalPrice}</p> */}
                   <p>selectted items are {position} item and Total number is {singleTotalPrice}</p>
+
+
                 </div>
               </form>
             </div>
-          </div>
-        )}
+          </div >
+        )
+        }
       />
-    </div>
+    </div >
   );
 }
 
